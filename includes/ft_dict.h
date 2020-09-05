c16a1212968a3f766204c30294f095d09ace9d2c
Abigail Dustin <adustin@et-f6.kzn.21-school.ru>
2020-09-05 16:26:12 +0300

    create search_dist.c && update main

--- includes/ft_dict.h
+++ includes/ft_dict.h
@@ -6,7 +6,7 @@
 /*   By: adustin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/05 13:59:05 by adustin           #+#    #+#             */
-/*   Updated: 2020/09/05 15:07:03 by adustin          ###   ########.fr       */
+/*   Updated: 2020/09/05 16:24:39 by adustin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,19 @@
 typedef struct	s_dict_small
 {
 	int value;
+	char *dict_value;
 	struct s_dist_small *left;
 	struct s_dist_small *right;
 }				t_dist_small;
 typedef struct	s_dict_big
 {
 	int value;
+	char *dict_value;
 	struct s_dist_big *left;
 	struct s_dist_big *right;
 }				t_dist_small;
 
-int search_dist(char *str);
+int search_dist(char *file_name);
 void ft_putchar(char c);
 void ft_putstr(char *str);
 
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: awave <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/05 13:56:39 by awave             #+#    #+#             */
-/*   Updated: 2020/09/05 15:08:32 by adustin          ###   ########.fr       */
+/*   Updated: 2020/09/05 16:24:32 by adustin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,23 @@
 
 int main(int argc, char *argv[])
 {
+	int file_exist;
+	char default_dict[13];
+
+	default_dict = "numbers.dict";
 	if (argc < 2 || argc > 3)
 		ft_putstr("Error\n");
 	else
-		ft_putstr("norme\n");
+	{
+		if (argc == 2)
+			file_exist = search_dirct(default_dict);
+		else
+			file_exist = search_dict(argv[2]);
+		if (file_exist)
+			ft_pustr("norme\n")
+		else
+			"Dict Error\n";
+	}
 	return 0;
 }
 
