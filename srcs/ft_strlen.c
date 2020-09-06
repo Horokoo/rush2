7a063de27b23cc9c4ec217a8d74d633686e7e97e
Abigail Dustin <adustin@et-f6.kzn.21-school.ru>
2020-09-06 20:28:55 +0300

    output beta

--- srcs/ft_strlen.c
+++ srcs/ft_strlen.c
@@ -1,32 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_dict.h                                          :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adustin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/09/05 13:59:05 by adustin           #+#    #+#             */
-/*   Updated: 2020/09/05 18:47:28 by adustin          ###   ########.fr       */
+/*   Created: 2020/09/06 16:09:43 by adustin           #+#    #+#             */
+/*   Updated: 2020/09/06 16:09:46 by adustin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_DICT_H
-# define FT_DICT_H
+#include "ft_dict.h"
 
-# include <stdlib.h>
-# include <unistd.h>
-# include <fcntl.h>
-# include <sys/types.h>
-# include <sys/stat.h>
+int ft_strlen(char *str)
+{
+    int len;
 
-char *g_file_array;
-
-int search_dict(char *file_name);
-void ft_putchar(char c);
-void ft_putstr(char *str);
-int ft_strlen(char *str);
-void check_dict(char *file_name);
-void digit_break(void);
-int gen_digit_struct(char *file_name);
-
-#endif
+    len = 0;
+    while (*(str++))
+        len++;
+    return (len);
+}
---
+++ srcs/output.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   output.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adustin <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/06 16:03:06 by adustin           #+#    #+#             */
+/*   Updated: 2020/09/06 16:03:17 by adustin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_dict.h"
+#include <stdio.h>
+
+char *ft_stronecopy(char *copy, char *str, int len, int one)
+{
+    int i;
+
+    i = 0;
+    copy = (char *)malloc(sizeof(char) * len + 1);
+    if (copy == NULL)
+    {
+        printf("error");
+        return "";
+    }
+    if (one)
+        copy[i++] = '1';
+    while (str[i])
+    {
+        copy[i] = str[i];
+        i++;
+    }
+    copy[len] = '\0';
+    return (copy);
+}
+
+void digit_output(char *str)
+{
+    int len;
+    char *copy;
+    char c;
+
+    len = ft_strlen(str);
+    if (len < 3)
+        printf("%s\n", str);
+    if (len == 3)
+    {
+        c = *str;
+        digit_output(&c);
+        copy = ft_stronecopy(copy, str, len, 1);
+        printf("%s\n", copy);
+        free(copy);
+    }
+    if (len > 3)
+    {
+        if (len % 3 == 2)
+        {
+            digit_output(ft_stronecopy(NULL, str, 2, 0));
+            copy = ft_stronecopy(copy, str, len - 1, 1);
+            printf("%s\n", copy);
+        }
+        if (len % 3 == 1)
+        {
+            c = *str;
+            digit_output(&c);
+            copy = ft_stronecopy(copy, str, len, 1);
+            printf("%s\n", copy);
+        }
+        if (len % 3 == 0)
+        {
+            c = *str;
+            digit_output(&c);
+            copy = ft_stronecopy(copy, str, 3, 1);
+            printf("%s\n", copy);
+            copy = ft_stronecopy(copy, str, len - 2, 1);
+            printf("%s\n", copy);
+        }
+        
+    }
+}
