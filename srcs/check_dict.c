b239920eecbc97b236fc179d123fb64b0530b83d
Shamil <70801690+tlegenda@users.noreply.github.com>
2020-09-06 15:37:11 +0300

    Add files via upload

--- srcs/check_dict.c
+++ srcs/check_dict.c
@@ -1,20 +1,71 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   check_dict.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: adustin <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/09/05 18:27:30 by adustin           #+#    #+#             */
-/*   Updated: 2020/09/05 18:47:25 by adustin          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
 #include "ft_dict.h"
 
-void check_dict(char *file_name)
+int ft_printable(char *str)
 {
-	if (!(gen_digit_struct(file_name)))
+	int i = 0;
+	while (str[i] == ' ')
+		i++;
+	if (!(str[i] >= 33 && str[i] <= 127))
+		return(0);
+	else
+		return (1);
+}
+
+void	ft_next_line(int *fd, int *i)
+{
+	char	buffer;
+
+	while (read(*fd, &buffer, 1))
+	{
+		if (buffer == '\n')
+			break ;
+	}
+	*i = 0;
+}
+
+int ft_dict_length(void)
+{
+	int		i;
+	int		j;
+	int		fd;
+	char	buf;
+
+	j = 0;
+	i = -1;
+	if((fd = open("numbers.dict", O_RDONLY)) == -1)
+	{
+		ft_putstr("Dict Error\n");
+		return (0);
+	}
+	else
+	{
+		while (read(fd, &buf, 1))
+		{
+			if (buf == ':')
+			{
+				while (read(fd, &buf, 1))
+				{
+					if (ft_printable(&buf) == 1)
+					{
+						j++;
+						break ;
+					}
+				}
+				ft_next_line(&fd, &i);
+			}
+			i++;
+		}
+		return(j);
+	}
+}
+
+int	length_check(void)
+{
+	int len_max;
+
+	len_max = ft_dict_length();
+	if (len_max < 41)
 		ft_putstr("Dict Error\n");
 	else
 		digit_break();
