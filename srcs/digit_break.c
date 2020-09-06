318965461dd1f8d434d25ac9ba09822729e11204
Alcestis Wave <awave@pr-d3.kzn.21-school.ru>
2020-09-06 22:38:28 +0300

    digit_break

--- srcs/digit_break.c
+++ srcs/digit_break.c
@@ -6,11 +6,23 @@
 /*   By: adustin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/05 18:39:38 by adustin           #+#    #+#             */
-/*   Updated: 2020/09/05 18:45:14 by adustin          ###   ########.fr       */
+/*   Updated: 2020/09/06 22:37:27 by awave            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_dict.h"
+
+void put_str(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+	write(1, &str[i], 1);
+		i++;
+	}
+}
 
 void digit_break(char * c)
 {
@@ -18,25 +30,32 @@ void digit_break(char * c)
 	int ic;
 	int j;
 	int k;
-	int v;
+	char v;
+	char *fg;
 
 	i = 0;
 	j = 0;
 	k = 0;
 	ic = 0;
+	while (c[k])
+	{
+		k++;
+	}
 	while (c[i])
 	{
+		fg = malloc(sizeof(char) * k);
+		fg[0] = c[i];
+		j = 1;
+		while (j < k)
+		{
+			fg[j] = '0';
+			j++;
+		}
+		fg[j] = '\0';
+		printf("%s\n", fg);
 		i++;
+		k--;
+		free(fg);
 	}
-	while (j < (i - 1) && (ic < i - 1))
-	{
-		k = j;
-		v = c[ic] - 48;
-		while (k++ < (i - 1))
-			v = v * 10;
-		digit_output(v);
-		ic++;
-		j++;
-	}
-	digit_output(c[ic]);
 }
+
